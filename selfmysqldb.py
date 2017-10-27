595cf98efcde9d799b997ec24e6592d6a5c538e1
kof0012 <fenhong.wang@163.com>
2017-10-27 11:51:38 +0800

    Update selfmysqldb.py
    change insert

--- selfmysqldb.py
+++ selfmysqldb.py
@@ -15,30 +15,29 @@ class SQL():
             self.conn = pymysql.connect(host = hosts,user = username,passwd = password,db = database,charset = charsets)
             self.cursor = self.conn.cursor()
             self.cursor.execute("set names "+charsets)
-            self.connection = True
         except Exception as e:
             print ("Cannot Connect To Mysql!/n",e)
+        else:
+            self.connection = True
 
     def escape(self,string):
         return '%s' % string
     #插入数据到数据库   
     def insert(self,tablename=None,**values):
-
-        if self.connection: 
-            tablename = self.escape(tablename)  
-            if values:
-                _keys = ",".join(self.escape(k) for k in values)
-                _values = ",".join(['%s']*len(values))
-                sql_query = "replace into %s (%s) values (%s)" % (tablename,_keys,_values)
-            else:
-                sql_query = "replace into %s default values" % tablename
+        tablename = self.escape(tablename)
+        if self.connection:
             try:
                 if values:
+                    _keys = ",".join(self.escape(k) for k in values)
+                    _values = ",".join(['%s']*len(values))
+                    sql_query = "replace into %s (%s) values (%s)" % (tablename,_keys,_values)
                     self.cursor.execute(sql_query,list(values.values()))
-                else:       
+                else:
+                    sql_query = "replace into %s default values" % tablename
                     self.cursor.execute(sql_query)
                 self.conn.commit()
                 return True
             except Exception as e:
                 print ("An Error Occured: ",e)
                 return False
+            
